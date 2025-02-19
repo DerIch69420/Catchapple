081eb1eb199163a655dbc5ccb4041f63f511c345
DerIch69420 <netroMorten@web.de>
2025-02-19 06:42:56 +0100

    Fixed code issue

--- main.lua
+++ main.lua
@@ -1,23 +1,20 @@
 -- main.lua
+local love = require("love")
 
-local player = require("Scripts.player")
+local player = require("src.player")
 
 function love.load()
-	love.window.setFullscreen(true)
+    love.window.setFullscreen(true)
 end
 
 function love.update(dt)
-	
-	if love.keyboard.isDown("a") then
-		player:move(-1000, dt)
-	elseif love.keyboard.isDown("d") then
-		player:move(1000, dt)
-	end
-
+    if love.keyboard.isDown("a") then
+        player:move(-1000, dt)
+    elseif love.keyboard.isDown("d") then
+        player:move(1000, dt)
+    end
 end
 
 function love.draw()
-	player:draw()
+    player:draw()
 end
-
-
--- src/player.lua
+++ src/player.lua
@@ -1,4 +1,5 @@
 -- player.lua
+local love = require("love")
 
 local Player = {}
 
@@ -7,11 +8,11 @@ Player.y = 1000
 Player.speed = 100
 
 function Player:move(dx, dt)
-	Player.x = Player.x + dx * dt
+    Player.x = Player.x + dx * dt
 end
 
 function Player:draw()
-	love.graphics.rectangle("fill", Player.x, Player.y, 50, 50)
+    love.graphics.rectangle("fill", Player.x, Player.y, 50, 50)
 end
 
 return Player
